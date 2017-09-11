43b9e68c8407e2da4387139b6916d1b09dff3bce
Adam Grayson <applead@aim.com>
2017-09-11 12:51:38 -0400

    deleted unused pages

--- src/index.js
+++ src/index.js
@@ -5,25 +5,10 @@ import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 
 import { Home } from './screens/home';
-import { Movies } from './screens/movies';
-import { MovieDetail } from './screens/movie-detail';
 
 render (
 	<Router>
-		<div>
-			<nav>
-				<ul>
-					<li><Link to="/">Home</Link></li>
-					<li><Link to="/movies">Movies</Link></li>
-				</ul>
-			</nav>
-
-			<hr/>
-
-			<Route exact path='/' component={ Home }/>
-			<Route exact path='/movies' component={ Movies }/>
-			<Route path='/movies/:id' component={ MovieDetail }/>
-		</div>
+		<Route exact path='/' component={ Home }/>
 	</Router>,
 	document.getElementById('root')
 );
